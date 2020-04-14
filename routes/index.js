656d8375b946dc4329730e85848844af3f8aecad
PaolaS2992 <huaripayta29.92@gmail.com>
2020-04-14 02:32:37 -0500

    agrega funcion test

--- routes/index.js
+++ routes/index.js
@@ -82,4 +82,23 @@ router.get('/profile', (req, res) => {
   res.send(req.headers.user);
 });
 
+// Hackaton Interna - Guardar respuesta de test.
+router.post('/test', (req, res) => {
+  const resTest = {
+    emailPostulante: req.headers.user.email,
+    tiempoRealizado: '15 min', // req.body.time
+    nombrePrueba: 'Estres laboral', // falta
+    resultados: req.body.resultados,
+  };
+
+  return collection('resultados')
+    .then((collectionResultados) => collectionResultados.insertOne(resTest))
+    .then((newTest) => {
+      res.send(newTest.ops[0]);
+    });
+});
+
+// Hackathon Interna - Mostrar respuestas test de todos postulantes.
+router.get('/dashboard', (req, res) => {});
+
 module.exports = router;
