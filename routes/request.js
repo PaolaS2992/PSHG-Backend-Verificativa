1bd6d64d77d05d12876ffb475703438dd95e4470
PaolaS2992 <huaripayta29.92@gmail.com>
2020-05-01 18:54:53 -0500

    modifica objArr massive

--- routes/request.js
+++ routes/request.js
@@ -4,14 +4,14 @@ const router = Router();
 // Fusion FrontEnd
 
 router.post('/massive', (req, res) => {
-    const { data } = req.body; // Validar nombre del arrObj con Maray.
+    const data = req.body; // Validar nombre del arrObj con Maray.
     // TODO - Sanear el Excel (Validar la data segun formato).
     // TODO - Agregarle el propietario (Relacionar que usuario de la covocatoria).
     let db;
     // const formatoJson = functionExcel.getExcelJson();
     collection('requestMassive')
       .then((dbCollection) => db = dbCollection)
-      .then(() => db.insertOne(req.body)) //req.body
+      .then(() => db.insertOne({data})) //req.body
       // .then(() => functionEmail.sendMasivoEmail(formatoJson))
       .then(() => res.send({ message:'Correos enviados!!!' }))
       .catch(err => console.log(err));
