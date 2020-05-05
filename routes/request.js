15f964d82c085744aae07dd1631ce9019c877e0e
PaolaS2992 <huaripayta29.92@gmail.com>
2020-05-04 19:33:02 -0500

    cambio then

--- routes/request.js
+++ routes/request.js
@@ -3,7 +3,7 @@ const collection = require('../connection/collection');
 const router = Router();
 // Fusion FrontEnd
 
-router.post('/massive', (req, res) => {
+/* router.post('/massive', (req, res) => {
     const data = req.body; // Validar nombre del arrObj con Maray.
     // TODO - Sanear el Excel (Validar la data segun formato).
     // TODO - Agregarle el propietario (Relacionar que usuario de la covocatoria).
@@ -12,6 +12,19 @@ router.post('/massive', (req, res) => {
     return collection('requestMassive')
       .then((dbCollection) => db = dbCollection)
       .then(() => db.insertOne(data)) //req.body
+      // .then(() => functionEmail.sendMasivoEmail(formatoJson))
+      .then(() => res.send({ message:'Correos enviados!!!' }))
+      .catch(err => console.log(err));
+  }); */
+
+  router.post('/massive', (req, res) => {
+    const data = req.body; // Validar nombre del arrObj con Maray.
+    // TODO - Sanear el Excel (Validar la data segun formato).
+    // TODO - Agregarle el propietario (Relacionar que usuario de la covocatoria).
+    let db;
+    // const formatoJson = functionExcel.getExcelJson();
+    return collection('requestMassive')
+      .then((dbCollection) => dbCollection.insertOne(data))
       // .then(() => functionEmail.sendMasivoEmail(formatoJson))
       .then(() => res.send({ message:'Correos enviados!!!' }))
       .catch(err => console.log(err));
