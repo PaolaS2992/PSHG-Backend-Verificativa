201f6f2595213f8b6a0f5b03f17ed9075f9fbeac
PaolaS2992 <huaripayta29.92@gmail.com>
2020-05-06 13:25:19 -0500

    integra la funcion sendEmail

--- routes/request.js
+++ routes/request.js
@@ -1,65 +1,56 @@
 const { Router } = require('express');
 const collection = require('../connection/collection');
+const functionEmail = require('../lib/libEmail');
 const router = Router();
-// Fusion FrontEnd
 
-/* router.post('/massive', (req, res) => {
-    const data = req.body; // Validar nombre del arrObj con Maray.
-    // TODO - Sanear el Excel (Validar la data segun formato).
-    // TODO - Agregarle el propietario (Relacionar que usuario de la covocatoria).
-    let db;
-    // const formatoJson = functionExcel.getExcelJson();
-    return collection('requestMassive')
-      .then((dbCollection) => db = dbCollection)
-      .then(() => db.insertOne(data)) //req.body
-      // .then(() => functionEmail.sendMasivoEmail(formatoJson))
-      .then(() => res.send({ message:'Correos enviados!!!' }))
-      .catch(err => console.log(err));
-  }); */
+router.post('/massive', (req, res) => {
+  // TODO - Sanear el Excel (Validar la data segun formato).
+  // TODO - Agregarle el propietario (Relacionar que usuario de la covocatoria).
+  const {
+    idUser,
+    fechaVigencia,
+    test,
+    candidatos
+  } = req.body;
 
-  router.post('/massive', (req, res) => {
-    // TODO - Sanear el Excel (Validar la data segun formato).
-    // TODO - Agregarle el propietario (Relacionar que usuario de la covocatoria).
-    const {
-      idUser,
-      fechaVigencia,
-      test,
-      candidatos
-    } = req.body;
+  if (!idUser || !fechaVigencia || !test || !candidatos){
+    return res.status(400).send({ message: 'Ingresar todos los datos' });
+  }
 
-    const newRequest = {
-      idUser: req.body.idUser,
-      fechaVigencia: req.body.fechaVigencia,
-      test: req.body.test,
-      candidatos: req.body.candidatos
-    };
+  const newRequest = {
+    idUser: req.body.idUser,
+    fechaVigencia: req.body.fechaVigencia,
+    test: req.body.test,
+    candidatos: req.body.candidatos
+  };
 
-    console.log('Soy req, body', req.body);
+  console.log('Soy req, body', req.body);
 
-    let db;
-    // const formatoJson = functionExcel.getExcelJson();
-    return collection('requestMassive')
-      .then((dbCollection) => db = dbCollection)
-      .then(() => db.insertOne(newRequest))
-      // .then(() => functionEmail.sendMasivoEmail(formatoJson))
-      .then((result) => {
-        res.send({
-          _id: result.ops[0]._id,
-          idUser: result.ops[0].idUser,
-          fechaVigencia: result.ops[0].fechaVigencia,
-          test: result.ops[0].test,
-          candidatos: result.ops[0].candidatos
-        })
-      }).catch(err => console.log(err));
-  });
+  let db;
+  // const formatoJson = functionExcel.getExcelJson();
+  return collection('requestMassive')
+    .then((dbCollection) => db = dbCollection)
+    .then(() => db.insertOne(newRequest))
+    .then(() => functionEmail.sendMasivoEmail(newRequest))
+    .then(() => res.send({ message: 'Correos enviados' }))
+    .catch(err => console.log(err));
+});
 
-  router.get('/massive', (req, res) => {
-    let db;
-    return collection('requestMassive')
-      .then((dbCollection) => db = dbCollection)
-      .then(() => db.find().toArray())
-      .then((result) => res.send(result))
-      .catch((err) => console.log(err));
-  });
+/* res.send({
+        _id: result.ops[0]._id,
+        idUser: result.ops[0].idUser,
+        fechaVigencia: result.ops[0].fechaVigencia,
+        test: result.ops[0].test,
+        candidatos: result.ops[0].candidatos
+      }) */
+
+router.get('/massive', (req, res) => {
+  let db;
+  return collection('requestMassive')
+    .then((dbCollection) => db = dbCollection)
+    .then(() => db.find().toArray())
+    .then((result) => res.send(result))
+    .catch((err) => console.log(err));
+});
 
 module.exports = router;
